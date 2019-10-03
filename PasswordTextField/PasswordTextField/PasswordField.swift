e9120568adbc3711a99f06a5ed3d32af24c77b7c
Robert Keffury <bobby.keffury@me.com>
2019-10-03 09:09:22 -0700

    Restarted project, added all features.

--- PasswordTextField/PasswordTextField/PasswordField.swift
+++ PasswordTextField/PasswordTextField/PasswordField.swift
@@ -38,12 +38,95 @@ class PasswordField: UIControl {
     private var strongView: UIView = UIView()
     private var strengthDescriptionLabel: UILabel = UILabel()
     
+    private let image1 = UIImage(named: "eyes-closed")
+    private let image2 = UIImage(named: "eyes-open")
+    
+    
     func setup() {
+        
         // Lay out your subviews here
         
-        addSubview(titleLabel)
+        backgroundColor = bgColor
+        
+        // Title Label
+        titleLabel.text = "ENTER PASSWORD"
+        titleLabel.font = labelFont
+        titleLabel.textColor = labelTextColor
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(titleLabel)
+        titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: standardMargin).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: standardMargin).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 20.0).isActive = true
+        
+        // Text Field
+        textField.layer.cornerRadius = 8.0
+        textField.layer.borderColor = textFieldBorderColor.cgColor
+        textField.layer.borderWidth = 2.0
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(textField)
+        textField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8.0).isActive = true
+        textField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: textFieldMargin).isActive = true
+        textField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: textFieldMargin).isActive = true
+        textField.heightAnchor.constraint(equalToConstant: textFieldContainerHeight).isActive = true
+      
+        // Show Hide Button
+        showHideButton.setImage(image1, for: .normal)
+        showHideButton.translatesAutoresizingMaskIntoConstraints = false
+        textField.addSubview(showHideButton)
+        showHideButton.trailingAnchor.constraint(equalTo: textField.trailingAnchor, constant: -10.0).isActive = true
+        showHideButton.centerYAnchor.constraint(equalTo: textField.centerYAnchor, constant: 0).isActive = true
+     
+
+        // Weak View
+        
+        weakView.backgroundColor = unusedColor
+        weakView.layer.cornerRadius = 2.0
+        weakView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(weakView)
+        weakView.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 15.0).isActive = true
+        weakView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: standardMargin).isActive = true
+        weakView.heightAnchor.constraint(equalToConstant: 6.0).isActive = true
+        weakView.widthAnchor.constraint(equalToConstant: 50.0).isActive = true
+       
+
+        // Medium View
+        
+        mediumView.backgroundColor = unusedColor
+        mediumView.layer.cornerRadius = 2.0
+        mediumView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(mediumView)
+        mediumView.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 15.0).isActive = true
+        mediumView.leadingAnchor.constraint(equalTo: weakView.trailingAnchor, constant: standardMargin).isActive = true
+        mediumView.heightAnchor.constraint(equalToConstant: 6.0).isActive = true
+        mediumView.widthAnchor.constraint(equalToConstant: 50.0).isActive = true
+      
+
+        // Strong View
+        
+        strongView.backgroundColor = unusedColor
+        strongView.layer.cornerRadius = 2.0
+        strongView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(strongView)
+        strongView.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 15.0).isActive = true
+        strongView.leadingAnchor.constraint(equalTo: mediumView.trailingAnchor, constant: standardMargin).isActive = true
+        strongView.heightAnchor.constraint(equalToConstant: 6.0).isActive = true
+        strongView.widthAnchor.constraint(equalToConstant: 50.0).isActive = true
+       
+
+        // Strength Description Label
+        strengthDescriptionLabel.text = "Too Weak"
+        strengthDescriptionLabel.font = labelFont
+        strengthDescriptionLabel.textColor = labelTextColor
+        strengthDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(strengthDescriptionLabel)
+        strengthDescriptionLabel.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 10.0).isActive = true
+        strengthDescriptionLabel.leadingAnchor.constraint(equalTo: strongView.trailingAnchor, constant: standardMargin).isActive = true
+        strengthDescriptionLabel.heightAnchor.constraint(equalToConstant: 15.0).isActive = true
+        strengthDescriptionLabel.widthAnchor.constraint(greaterThanOrEqualToConstant: 50.0).isActive = true
+
+       
     }
+    
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
